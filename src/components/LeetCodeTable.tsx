0ed2d480fa01a7919e418499a71026c67124f03e
yidnekachew tebeje <yidnekachewtebeje@gmail.com>
2023-04-08 07:35:33 +0300

    update no cache

--- src/components/LeetCodeTable.tsx
+++ src/components/LeetCodeTable.tsx
@@ -3,14 +3,11 @@ import { leetcoderes } from '../interfaces/leetcoderes';
 import { useState, useEffect, use } from "react";
 import { createColumnHelper, flexRender, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, getSortedRowModel, PaginationState, RowSelection, SortingState, useReactTable } from '@tanstack/react-table';
 import React from 'react';
-import Link from 'next/link';
+
 import { DebouncedInput } from './DebounceInput';
-import Pagination from './Pagination';
-import Paginations from './Pagination';
 export const dynamic = "force-dynamic";
 async function getData(): Promise<leetcoderes[]>{
-    const url = new URL('http://localhost:3000/api');
-    const data = await fetch(url, {
+    const data = await fetch("/api", {
         cache: 'no-store'
     });
     let res: {
