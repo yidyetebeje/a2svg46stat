97a900c07ad38308e817867d49825f5601580e5e
yidnekachew tebeje <yidnekachewtebeje@gmail.com>
2023-04-13 20:39:19 +0300

    error boundary fix

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -19,7 +19,6 @@ export default function Error({
             <h2>Something went wrong!</h2>
             <button
                 onClick={
-                    // Attempt to recover by trying to re-render the segment
                     () => reset()
                 }
             >
